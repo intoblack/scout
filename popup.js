93f709b05198cd12a45bc2fd8705107a55587174
intoblack <intoblack86@gmail.com>
2014-12-01 20:30:21 +0800

    更改网页页面格式 ， 利用bootstarp

--- popup.js
+++ popup.js
@@ -1,96 +1,62 @@
 function onAnchorClick(event) {
-   chrome.tabs.create({
-     selected: true,
-      url: event.srcElement.href
-    });
-    return false;
-  }
-  
+  chrome.tabs.create({
+    selected: true,
+    url: event.srcElement.href
+  });
+  return false;
+}
 
-  function buildPopupDom(divName, data) {
-    var popupDiv = document.getElementById(divName);
-  
-    var ul = document.createElement('ul');
-    popupDiv.appendChild(ul);
-  
-    for (var i = 0, ie = data.length; i < ie; ++i) {
-      var a = document.createElement('a');
-      a.href = data[i];
-      a.appendChild(document.createTextNode(data[i]));
-      a.addEventListener('click', onAnchorClick);
-  
-      var li = document.createElement('li');
-      li.appendChild(a);
-  
-      ul.appendChild(li);
-    }
-  }
-  
 
-  function buildTypedUrlList(divName) {
-  
-    var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 1;
-    var beforeTime = (new Date).getTime() - microsecondsPerWeek;
-  
-    var numRequestsOutstanding = 0;
-  
-    chrome.history.search({
-        'text': '',            
-        'startTime': beforeTime  
-      },
-      function(HistoryItem ) {
-        for (var i = 0; i < HistoryItem .length; ++i) {
-          var url = HistoryItem[i].url;
-          var processVisitsWithUrl = function(url) {
-            return function(visitItems) {
-              processVisits(url, visitItems);
-            };
-          };
-          chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
-          numRequestsOutstanding++;
-        }
-        if (!numRequestsOutstanding) {
-          onAllVisitsProcessed();
-        }
+function buildPopupDom(divName, data) {
+  var popupDiv = document.getElementById(divName);
+  var table = document.createElement('table');
+  table.className = "table table-hover";
+  for (var i = 0, ie = data.length; i < ie; ++i) {
+    var tr = document.createElement('tr');
+    tr.className = "info";
+    var td = document.createElement('td')
+    td.className = "active";
+
+    td.appendChild(document.createTextNode(data[i]));
+
+    tr.appendChild(td);
+    var td1 = document.createElement('td');
+    td1.className = "warn";
+    td1.appendChild(document.createTextNode("100%"));
+    tr.appendChild(td1);
+    table.appendChild(tr);
+  }
+  popupDiv.appendChild(table);
+}
+
+var urlToCount = {};
+
+function buildTypedUrlList(divName) {
+
+  var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 1;
+  var beforeTime = (new Date).getTime() - microsecondsPerWeek;
+
+  var numRequestsOutstanding = 0;
+
+  chrome.history.search({
+      'text': '',
+      'startTime': beforeTime
+    },
+    function(HistoryItem) {
+      for (var i = 0; i < HistoryItem.length; ++i) {
+        var url = HistoryItem[i].url;
+
       });
-  
-  
+  }
+}
 
-    var urlToCount = {};
-  
-  
-    var processVisits = function(url, visitItems) {
-      for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-        if (visitItems[i].transition != 'typed') {
-          continue;
-        }
-  
-        if (!urlToCount[url]) {
-          urlToCount[url] = 0;
-        }
-  
-        urlToCount[url]++;
-      }
 
-      if (!--numRequestsOutstanding) {
-        onAllVisitsProcessed();
-      }
-    };
-  
-   var onAllVisitsProcessed = function() {
-     urlArray = [];
-     for (var url in urlToCount) {
-       urlArray.push(url);
-     }
-     urlArray.sort(function(a, b) {
-       return urlToCount[b] - urlToCount[a];
-     });
- 
-     buildPopupDom(divName, urlArray.slice(0, 10));
-   };
- }
- 
- document.addEventListener('DOMContentLoaded', function () {
-  
-   buildTypedUrlList("history");
- });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  buildTypedUrlList("container-fluid");
+});
